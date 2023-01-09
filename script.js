6270dde09b28e4f4f532c5db1d0102b2cb0f4e1a
Exequiel Bobruk <bobrukfs@gmail.com>
2023-01-08 21:07:01 -0300

    Optimizacion codigo header

--- script.js
+++ script.js
@@ -9,9 +9,13 @@ function activate() {
   
     if (menuButtonCondition === false) {
         menuNav[0].setAttribute("class","menu active");
+        barraMiddle[0].style.background = "white";
+        menuButton[0].setAttribute("class", "blanco header__button");
         menuButtonCondition = true;
     } else {
         menuNav[0].setAttribute("class", "menu")
+        barraMiddle[0].style.background = "#4b5780";
+        menuButton[0].setAttribute("class", "primario header__button");
         menuButtonCondition = false;
     }
 
