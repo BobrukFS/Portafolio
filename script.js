15383b33c9cef524f384b8e00cbf2626a53e6567
Exequiel Bobruk <bobrukfs@gmail.com>
2023-02-26 00:51:37 -0300

    Header e introduce hechos

--- script.js
+++ script.js
@@ -3,24 +3,64 @@
 let menuButton = document.getElementsByClassName("header__button");
 let menuButtonCondition = false;
 let menuNav = document.getElementsByClassName("menu");
-let barraMiddle = document.getElementsByClassName("header__buttonicon");
+let barraMiddle = document.getElementsByClassName("button__icon");
 
-function activate() {
-  
+
+menuButton[0].addEventListener("click", (e) => {
     if (menuButtonCondition === false) {
         menuNav[0].setAttribute("class","menu active");
         barraMiddle[0].style.background = "white";
-        menuButton[0].setAttribute("class", "blanco header__button");
+        menuButton[0].setAttribute("class", "on header__button");
         menuButtonCondition = true;
     } else {
         menuNav[0].setAttribute("class", "menu")
-        barraMiddle[0].style.background = "#4b5780";
-        menuButton[0].setAttribute("class", "primario header__button");
+        barraMiddle[0].style.background = "#333333";
+        menuButton[0].setAttribute("class", "off header__button");
         menuButtonCondition = false;
     }
+    e.stopPropagation();
+});
 
+menuButton[0].addEventListener("click", (e)=>{
+    
+});
+
+// Cartas
+
+let carta = document.getElementsByClassName("card");
+let fig = document.getElementsByClassName("card__figcaption");
+let img = document.getElementsByClassName("card__img");
+let active = document.getElementsByClassName("active");
+let active1 = document.getElementsByClassName("active1");
+let icons = document.getElementsByClassName("card__nav");
+
+for (let i = 0; i < carta.length; i++) {
+
+    img[i].addEventListener("click", (e)=> {
+        if ((active.length > 0 || active1.length > 0) && carta[i] !== active1[0]) {
+            active[0].classList.remove("active");
+            active1[0].classList.remove("active1");
+            fig[i].classList.add("active");
+            carta[i].classList.add("active1");
+        } else {
+            fig[i].classList.add("active");
+            carta[i].classList.add("active1");
+        }
+        e.stopPropagation();
+    });
+    
 }
 
+for(let i = 0 ; i < icons.length; i++){
+  icons[i].addEventListener("click", (e)=>{  
+        e.stopPropagation();
+    })  
+}
 
-menuButton[0].addEventListener("click", activate);
+window.addEventListener("click", function(){
+    if (active.length > 0){
+        active[0].classList.remove("active");
+        active1[0].classList.remove("active1");
+    }
 
+});
